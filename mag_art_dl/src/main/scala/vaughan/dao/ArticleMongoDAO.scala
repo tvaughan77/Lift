c086fd6416c398be84cf3a12a166dfe2fa8075f3
tom.vaughan <tom_vaughan@hotmail.com>
2012-03-08 11:10:43 -0500

    First version of a bit of code that loads the Casbah wrapper around the Java MongoDB driver and creates a localhost connection
    ** Note - I haven't figured out test-level dependency injection yet, so the ArticleMongoDBSuite test demands a localhost:27017 mongodb instance running

--- mag_art_dl/src/main/scala/vaughan/dao/ArticleMongoDAO.scala
+++ mag_art_dl/src/main/scala/vaughan/dao/ArticleMongoDAO.scala
@@ -1,6 +1,7 @@
 package vaughan.dao
 
 import vaughan.model.Article
+import com.mongodb.casbah.Imports._
 
 /**
  * <p>A Mongo-backed DAO for Article objects</p>
--- mag_art_dl/src/test/scala/vaughan/dao/ArticleMongoDBSuite.scala
+++ mag_art_dl/src/test/scala/vaughan/dao/ArticleMongoDBSuite.scala
@@ -5,6 +5,7 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
 import org.junit.Assert._
 import java.text.SimpleDateFormat
+import com.mongodb.casbah.Imports._
 
 
 @RunWith(classOf[JUnitRunner])
@@ -13,4 +14,10 @@ class ArticleMongoDBSuite extends FunSuite {
   test("Find all returns a couple mock articles") {
     assertEquals(2, ArticleMongoDAO.findAll().size)
   }
+  
+  test("test connection to localhost mongodb") {
+    val mongoConn = MongoConnection()
+    val mongoDB = mongoConn("casbah_test")
+  }
+  
 }
