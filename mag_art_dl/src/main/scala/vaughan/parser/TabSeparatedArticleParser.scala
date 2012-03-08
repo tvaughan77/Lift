1d69d173a1c9637a53404c6192917427db9be090
tom.vaughan <tom_vaughan@hotmail.com>
2012-03-08 08:49:17 -0500

    Moving the static parseLine from being defined in an companion object to be encapsulated within the class's function instead

--- mag_art_dl/src/main/scala/vaughan/parser/TabSeparatedArticleParser.scala
+++ mag_art_dl/src/main/scala/vaughan/parser/TabSeparatedArticleParser.scala
@@ -30,38 +30,37 @@ class TabSeparatedArticleParser(val hasHeaderRow: Boolean) extends ArticleParser
 
     val articles = scala.collection.mutable.Set.empty[Article]
     for(line <- lines) 
-      articles += TabSeparatedArticleParser.parseLine(line)
+      articles += parseLine(line)
+    
+    
+    /*
+     * @param line a single line containing exactly 7 tokens of tab-separated information in it, with this spec:
+     * <ol>
+     *  <li>Name of the publication</li>
+     *  <li>The edition of the publication, in date format like "Mar 2011" or "Aug 2009"</li>
+     *  <li>The first page of the article, parseable as an integer</li>
+     *  <li>The title of the article</li>
+     *  <li>An optional description of the article.</li>
+     *  <li>An optional string containing notes about the article</li>
+     *  <li>A comma separated list of categories (meta-data) describing the article</li>
+     * </ol>
+     */
+    def parseLine(line: String): Article = {
+      val tokens = line.split('\t')
+      debug("parsing input line '%s'", line)
+      require(7 == tokens.size, {error("Input line only had %d tokens", tokens.size)})
+
+      val edition = Article.dFormat.parse(tokens(1))
+      val page = tokens(2).toInt
+      val categories = tokens(6).split(",").toList map(_.trim)
+
+      debug("categories = %s", categories.mkString)
+
+      new Article(tokens(0), edition, page, tokens(3), tokens(4), tokens(5), categories)
+    }
+    
     
     articles.toList
   }
 }
 
-
-object TabSeparatedArticleParser extends LogHelper {
-  
-  /**
-   * @param line a single line containing exactly 7 tokens of tab-separated information in it, with this spec:
-   * <ol>
-   *  <li>Name of the publication</li>
-   *  <li>The edition of the publication, in date format like "Mar 2011" or "Aug 2009"</li>
-   *  <li>The first page of the article, parseable as an integer</li>
-   *  <li>The title of the article</li>
-   *  <li>An optional description of the article.</li>
-   *  <li>An optional string containing notes about the article</li>
-   *  <li>A comma separated list of categories (meta-data) describing the article</li>
-   * </ol>
-   */
-  private def parseLine(line: String): Article = {
-    val tokens = line.split('\t')
-    debug("parsing input line '%s'", line)
-    require(7 == tokens.size, {error("Input line only had %d tokens", tokens.size)})
-    
-    val edition = Article.dFormat.parse(tokens(1))
-    val page = tokens(2).toInt
-    val categories = tokens(6).split(",").toList map(_.trim)
-    
-    debug("categories = %s", categories.mkString)
-    
-    new Article(tokens(0), edition, page, tokens(3), tokens(4), tokens(5), categories)
-  }
-}
